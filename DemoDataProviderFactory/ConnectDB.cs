ef67424f38a9281d34e9aaded48df9ac9bed2027
PPDUC <PPDUC@PPDUC>
2023-10-18 10:22:46 +0700

    Add project files.

---
+++ DemoDataProviderFactory/ConnectDB.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoDataProviderFactory
+{
+    public class ConnectDB
+    {
+        public ConnectDB() { }  
+        public static string GetConnectionString()
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                                        .SetBasePath(Directory.GetCurrentDirectory())
+                                        .AddJsonFile("appsettings.json", true, true)
+                                        .Build();
+            var strConnection = config["ConnectionString:MyStoreDB"];
+            return strConnection;
+        }
+    }
+}
